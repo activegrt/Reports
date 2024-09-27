578e32c791339d44aebe360ad2800fcdcb31e20e
activegrt <127025694+activegrt@users.noreply.github.com>
2024-09-27 22:41:50 +0400

    Create navItems.js

---
+++ navItems.js
@@ -0,0 +1,5 @@
+const navItems = [
+    { name: 'Home', page: 'equity_intraday_recommendation.html' },
+    { name: 'About', page: 'trade_signals.html' },
+    { name: 'Services', page: 'crypto_trigger_summary.html' }
+];
