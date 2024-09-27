8baf36507b1fdb2b159536f372c6747e0cfcd141
activegrt <127025694+activegrt@users.noreply.github.com>
2024-09-27 22:45:21 +0400

    Update navItems.js

--- navItems.js
+++ navItems.js
@@ -1,5 +1,7 @@
 const navItems = [
     { name: 'Home', page: 'equity_intraday_recommendation.html' },
     { name: 'About', page: 'trade_signals.html' },
+    { name: 'Buy', page: 'buy_new_oportunity.html' },
+    { name: 'Sell', page: 'sell_new_oportunity.html' },
     { name: 'Services', page: 'crypto_trigger_summary.html' }
 ];
